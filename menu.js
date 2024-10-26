540a7ad94f8c76114060bc2826d342620e2b8fa8
Mateus0Vasconcelos <161360131+Mateus0Vasconcelos@users.noreply.github.com>
2024-10-25 22:59:37 -0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
-let buttonEl = document.querySelector('#alterna-menu');
-let bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('#alterna-menu');
+const bodyEl = document.querySelector('body');
 
 function alternaMenu() {
     bodyEl.classList.toggle('menu-visivel');
